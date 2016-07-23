ed81439f98a0546245b80cad5d2848a0bc4b5d6e
Warin <info@kingwarin.de>
2016-07-23 11:37:25 +0200

    Add single-switch for buttons and comments.

--- controls.py
+++ controls.py
@@ -1,22 +1,52 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 class Button:
-    def __init__(self,pin,callback):
+    ''' Simple Button
+
+    Button needs to know which GPIO-pin it should listen to,
+    what to do if it's pressed and can optionally be setup
+    so it only triggers once it's pressed or as long as it's
+    pressed.
+    '''
+    def __init__(self,pin,callback,single=True):
         self.pin = pin
         self.callback = callback
         self.oldButton = 1
+        self.lastPressed = self.current_millis()
+        self.single = single
         GPIO.setup(self.pin, GPIO.IN)
+
+    def current_millis(self):
+        return int(round(time.time() * 1000))
 
     def read(self):
         button = GPIO.input(self.pin)
-        if button == False and self.oldButton:
-            self.callback()
-        self.oldButton = button
+        now = self.current_millis()
+        if self.single:
+            if button == False and self.oldButton:
+                self.callback()
+            self.oldButton = button
+        else:
+            if button == False and (now - self.lastPressed > 150):
+                self.callback()
+                self.lastPressed = now
 
 class RotaryEnc:
+    ''' RotaryEncoder with integrated switch
+
+    Utilizies the Button-Class for the integrated switch
+    Needs to know both GPIO-pins for the rotaryEncoder,
+    the GPIO-pin for the swich and callbacks for both the
+    encoder and the switch.
+
+    Note: The integrated switch is set to trigger just once it's
+    pressed. This is due to the rotaryEncoders I use (KY-040)
+    will multi-trigger even with debouncing.
+    '''
     def __init__(self,PinA,PinB,button,rotaryCallback,buttonCallback):
         self.PinA = PinA
         self.PinB = PinB
